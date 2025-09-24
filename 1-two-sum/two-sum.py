6e9d2dc7027211eaa18c034ef6222b75b5892022
Sai Prajwal Bhagavatula <prajwalbhag9@gmail.com>
2025-09-23 20:20:39 -0500

    Time: 5209 ms (5.00%) | Memory: 13.3 MB (47.86%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #prajwal
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if j == i:
+                    continue
+                if nums[i] + nums[j] == target:
+                    return [i, j]
